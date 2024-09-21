dc39bf7c2d681bc3e02c95eebf99da2e86cdf869
Paul Water <p.r.water@setbased.nl>
2024-09-21 13:43:30 +0200

    Align with changes in dependencies.

--- src/TestStore.php
+++ src/TestStore.php
@@ -15,20 +15,16 @@ class TestStore extends SqlitePdoDataLayer
    * Insert a base URL.
    *
    * @param string|null $pBulUrl The base URL to be inserted.
+   *                             text
    *
    * @return int
    */
   public function tstMonkeyBaseUrlInsert(?string $pBulUrl): int
   {
-    $replace = [':p_bul_url' => $this->quoteVarchar($pBulUrl)];
+    $replace = [':p_bul_url' => $this->quoteText($pBulUrl)];
     $query   = <<< EOT
-insert into TST_MONKEY_BASE_URL( bul_url
-,                                bul_pages
-,                                bul_crawled )
-values( :p_bul_url
-,       0
-,       0)
-;
+insert into TST_MONKEY_BASE_URL(bul_url, bul_pages, bul_crawled)
+values (:p_bul_url, 0, 0);
 EOT;
     $query = str_repeat(PHP_EOL, 7).$query;
 
@@ -46,18 +42,17 @@ EOT;
   {
     $query = <<< EOT
 select t2.url_id
-,      t2.bul_id
-,      t2.url_url
-,      t2.url_id_referrer
-from   TST_MONKEY_BASE_URL t1
-join   TST_MONKEY_URL      t2  on  t2.bul_id = t1.bul_id
-where  t1.bul_crawled < t1.bul_pages
-and    t1.bul_crawled < 10
-and    t2.url_status  is null
+     , t2.bul_id
+     , t2.url_url
+     , t2.url_id_referrer
+from TST_MONKEY_BASE_URL t1
+join TST_MONKEY_URL      t2 on t2.bul_id = t1.bul_id
+where t1.bul_crawled < t1.bul_pages
+  and t1.bul_crawled < 10
+  and t2.url_status is null
 order by t1.bul_crawled
-,        random()
-limit 10
-;
+       , random()
+limit 10;
 EOT;
     $query = str_repeat(PHP_EOL, 5).$query;
 
@@ -69,21 +64,21 @@ EOT;
    * Selects whether a URL exists.
    *
    * @param string|null $pUrlUrl The URL.
+   *                             text
    *
    * @return bool
    */
   public function tstMonkeyUrlExists(?string $pUrlUrl): bool
   {
-    $replace = [':p_url_url' => $this->quoteVarchar($pUrlUrl)];
+    $replace = [':p_url_url' => $this->quoteText($pUrlUrl)];
     $query   = <<< EOT
 select 1
-from   TST_MONKEY_URL
-where  url_url = :p_url_url
-and    ifnull(url_method, 'GET') = 'GET'
-limit 1
-;
+from TST_MONKEY_URL
+where url_url = :p_url_url
+  and ifnull(url_method, 'GET') = 'GET'
+limit 1;
 EOT;
-    $query = str_repeat(PHP_EOL, 8).$query;
+    $query = str_repeat(PHP_EOL, 7).$query;
 
     return !empty($this->executeSingleton0($query, $replace));
   }
@@ -93,6 +88,7 @@ EOT;
    * Selects the details of a URL.
    *
    * @param int|null $pUrlId The ID of the URL.
+   *                         int
    *
    * @return array
    */
@@ -101,11 +97,10 @@ EOT;
     $replace = [':p_url_id' => $this->quoteInt($pUrlId)];
     $query   = <<< EOT
 select url_id
-,      url_url
-,      url_id_referrer
-from   TST_MONKEY_URL
-where  url_id = :p_url_id
-;
+     , url_url
+     , url_id_referrer
+from TST_MONKEY_URL
+where url_id = :p_url_id;
 EOT;
     $query = str_repeat(PHP_EOL, 7).$query;
 
@@ -122,12 +117,12 @@ EOT;
   {
     $query = <<< EOT
 select url_id
-,      bul_id
-,      url_url
-,      url_id_referrer
-from   TST_MONKEY_URL
-where  url_method    = 'GET'
-and    url_has_forms = 1
+     , bul_id
+     , url_url
+     , url_id_referrer
+from TST_MONKEY_URL
+where url_method = 'GET'
+  and url_has_forms = 1
 order by random()
 EOT;
     $query = str_repeat(PHP_EOL, 5).$query;
@@ -140,27 +135,26 @@ EOT;
    * Insert a URL.
    *
    * @param string|null $pUrlUrl        The URL to be inserted.
+   *                                    text
    * @param int|null    $pBulId         The ID of the base URL of the URI to be inserted.
+   *                                    int
    * @param int|null    $pUrlIdReferrer The ID of the URI the is referring to the URI.
+   *                                    int
    *
    * @return int
    */
   public function tstMonkeyUrlInsert(?string $pUrlUrl, ?int $pBulId, ?int $pUrlIdReferrer): int
   {
-    $replace = [':p_url_url' => $this->quoteVarchar($pUrlUrl), ':p_bul_id' => $this->quoteInt($pBulId), ':p_url_id_referrer' => $this->quoteInt($pUrlIdReferrer)];
+    $replace = [':p_url_url' => $this->quoteText($pUrlUrl),
+                ':p_bul_id' => $this->quoteInt($pBulId),
+                ':p_url_id_referrer' => $this->quoteInt($pUrlIdReferrer)];
     $query   = <<< EOT
-insert into TST_MONKEY_URL( url_url
-,                           bul_id
-,                           url_id_referrer )
-values( :p_url_url
-,       :p_bul_id
-,       :p_url_id_referrer)
-;
+insert into TST_MONKEY_URL(url_url, bul_id, url_id_referrer)
+values (:p_url_url, :p_bul_id, :p_url_id_referrer);
 
 update TST_MONKEY_BASE_URL
-set    bul_pages = bul_pages + 1
-where  bul_id = :p_bul_id
-;
+set bul_pages = bul_pages + 1
+where bul_id = :p_bul_id;
 EOT;
     $query = str_repeat(PHP_EOL, 9).$query;
 
@@ -178,42 +172,38 @@ EOT;
   {
     $query = <<< EOT
 select url_status
-,      sum(url_count) as url_count
-from
-(
-  select '1xx' as url_status
-  ,       0    as url_count
+     , sum(url_count) as url_count
+from ( select '1xx' as url_status
+            , 0     as url_count
 
-  union all
+       union all
 
-  select '2xx'
-  ,       0
+       select '2xx'
+            , 0
 
-  union all
+       union all
 
-  select '3xx'
-  ,       0
+       select '3xx'
+            , 0
 
-  union all
+       union all
 
-  select '4xx'
-  ,       0
+       select '4xx'
+            , 0
 
-  union all
+       union all
 
-  select '5xx'
-  ,       0
+       select '5xx'
+            , 0
 
-  union all
+       union all
 
-  select '' || (url_status / 100) || 'xx'
-  ,      count(*)
-  from   TST_MONKEY_URL
-  where  url_status is not null
-  group by 1
-) t
-group by 1
-;
+       select '' || (url_status / 100) || 'xx'
+            , count(*)
+       from TST_MONKEY_URL
+       where url_status is not null
+       group by 1 ) t
+group by 1;
 EOT;
     $query = str_repeat(PHP_EOL, 5).$query;
 
@@ -225,34 +215,49 @@ EOT;
    * Updates a URL.
    *
    * @param int|null    $pUrlId        The ID of the URL.
+   *                                   int
    * @param int|null    $pBulId        The ID of the base URL.
+   *                                   int
    * @param string|null $pUrlMethod    The used method.
+   *                                   text
    * @param int|null    $pUrlStatus    The status of the URL.
+   *                                   int
    * @param string|null $pUrlLocation  The location of redirect header.
+   *                                   text
    * @param string|null $pUrlTitle     The title of the URL.
+   *                                   text
    * @param int|null    $pUrlHasForms  Whether the document has forms.
+   *                                   int
    * @param string|null $pUrlSubmitted The submitted values.
+   *                                   text
    * @param string|null $pUrlSource    The source of the URL.
+   *                                   text
    */
   public function tstMonkeyUrlUpdate(?int $pUrlId, ?int $pBulId, ?string $pUrlMethod, ?int $pUrlStatus, ?string $pUrlLocation, ?string $pUrlTitle, ?int $pUrlHasForms, ?string $pUrlSubmitted, ?string $pUrlSource): void
   {
-    $replace = [':p_url_id' => $this->quoteInt($pUrlId), ':p_bul_id' => $this->quoteInt($pBulId), ':p_url_method' => $this->quoteVarchar($pUrlMethod), ':p_url_status' => $this->quoteInt($pUrlStatus), ':p_url_location' => $this->quoteVarchar($pUrlLocation), ':p_url_title' => $this->quoteVarchar($pUrlTitle), ':p_url_has_forms' => $this->quoteInt($pUrlHasForms), ':p_url_submitted' => $this->quoteVarchar($pUrlSubmitted), ':p_url_source' => $this->quoteVarchar($pUrlSource)];
+    $replace = [':p_url_id' => $this->quoteInt($pUrlId),
+                ':p_bul_id' => $this->quoteInt($pBulId),
+                ':p_url_method' => $this->quoteText($pUrlMethod),
+                ':p_url_status' => $this->quoteInt($pUrlStatus),
+                ':p_url_location' => $this->quoteText($pUrlLocation),
+                ':p_url_title' => $this->quoteText($pUrlTitle),
+                ':p_url_has_forms' => $this->quoteInt($pUrlHasForms),
+                ':p_url_submitted' => $this->quoteText($pUrlSubmitted),
+                ':p_url_source' => $this->quoteText($pUrlSource)];
     $query   = <<< EOT
 update TST_MONKEY_URL
-set    url_method    = :p_url_method
-,      url_status    = :p_url_status
-,      url_location  = :p_url_location
-,      url_title     = :p_url_title
-,      url_has_forms = :p_url_has_forms
-,      url_submitted = :p_url_submitted
-,      url_source    = :p_url_source
-where url_id = :p_url_id
-;
+set url_method    = :p_url_method
+  , url_status    = :p_url_status
+  , url_location  = :p_url_location
+  , url_title     = :p_url_title
+  , url_has_forms = :p_url_has_forms
+  , url_submitted = :p_url_submitted
+  , url_source    = :p_url_source
+where url_id = :p_url_id;
 
 update TST_MONKEY_BASE_URL
-set    bul_crawled = bul_crawled + 1
-where  bul_id = :p_bul_id
-;
+set bul_crawled = bul_crawled + 1
+where bul_id = :p_bul_id;
 EOT;
     $query = str_repeat(PHP_EOL, 15).$query;
 
